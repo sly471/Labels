e8a0e70a7be650baf14f0c14c6bacb77c717aeb0
mvvej1 <eddie.joel.7@gmail.com>
2018-08-14 13:58:28 -0400

    Currently generates max 4 labels with their barcode in code128 format.

--- labelG2.php
+++ labelG2.php
@@ -15,16 +15,22 @@
         // $pdf = new PDF_Label('L7163');
         //                               Dimensions    w   h                                                     columns   rows 
         // $pdf = new PDF_Label(array('paper-size'=>array(99, 95), 'metric'=>'mm', 'marginLeft'=>1, 'marginTop'=>1, 'NX'=>2, 'NY'=>4, 'SpaceX'=>3, 'SpaceY'=>3, 'width'=>44, 'height'=>21, 'font-size'=>6));
-         $pdf = new PDF_Label(array('paper-size'=>array(96.52, 46.90), 'metric'=>'mm', 'marginLeft'=>0, 'marginTop'=>0, 'NX'=>4, 'NY'=>1, 'SpaceX'=>3, 'SpaceY'=>0, 'width'=>21, 'height'=>44, 'font-size'=>9));
-
+        $pdf = new PDF_Label(array('paper-size'=>array(96.52, 46.90), 'metric'=>'mm', 'marginLeft'=>0, 'marginTop'=>0, 'NX'=>4, 'NY'=>1, 'SpaceX'=>3, 'SpaceY'=>0, 'width'=>21, 'height'=>44, 'font-size'=>8));;
         $pdf->AddPage('L');
-
+        
         // Print labels
-        for($i=1;$i<=4;$i++) {
-            $text = sprintf("%s\n%s\n%s\n%s %s, %s", "Melaza $i", 'TWO STRANDS CLOVER NECKLACE', '056403841', '409901948204', 'GREY', '$10.90');
+        for($i=0;$i<4;$i++) {
+            $pdf->SetFontSize(8);
+            $text = sprintf("%s\n%s\n%s, %s", "Valija Gitana", 'TWO STRANDS CLOVER NECKLACE', 'GREY', '$10.90');
+            $code='12345678901234567890';
             $pdf->Add_Label($text);
+            $pdf->CODE128(4+$i*24, 30, $code, 15, 10, 15);
+            $pdf->SetFontSize(4);
+            $pdf->SetXY(2.5+$i*24,15);
+            $pdf->Write(51.5,$code);
+            
         }
 
         $pdf->Output();
-    ?>
 
+        ?>
