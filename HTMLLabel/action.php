b86f978c670e32265dfd4c46841cf39c334d4e1d
mvvej1 <eddie.joel.7@gmail.com>
2018-08-16 12:40:46 -0400

    Directory added creates labels online when user puts the information(Next step: Create and Connect database to program to recieve data from the database)

---
+++ HTMLLabel/action.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+You are <?php echo (int)$_POST['age']; ?> years old.
+</body>
+</html>
---
+++ HTMLLabel/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="labelG2.php" method="post">
+Order: <input type="number" name="order" min = 1 ><br>
+Name of Item:  <input type="text" name="name"><br>
+Item Code: <input type="text" name="code" min=20 max=20><br>
+Price: <input type="text" name="price"><br>
+Color: <input type="text" name="color"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
